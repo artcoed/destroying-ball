ec7fcaa4d0c1681e1e4994da836a6ace42d4f318
no_artyom <90335472+ligers2@users.noreply.github.com>
2022-07-10 01:31:38 +0500

    Update DefaultEnemiesFactory.cs

--- Code/Core/Tower/Factory/DefaultEnemiesFactory.cs
+++ Code/Core/Tower/Factory/DefaultEnemiesFactory.cs
@@ -4,11 +4,8 @@ public class DefaultEnemiesFactory : MonoBehaviour, IPoolFactory<Enemy>
 {
     [SerializeField] private Enemy _prefab = null;
 
-    private int i;
-
     public Enemy Create()
     {
-        print($"Врагов: {++i}");
         return Instantiate(_prefab);
     }
 }
