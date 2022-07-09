2e8926908a9d7fa54f79245e91156e1109da6e95
no_artyom <90335472+ligers2@users.noreply.github.com>
2022-07-10 01:31:51 +0500

    Update DefaultPlatformsFactory.cs

--- Code/Core/Tower/Factory/DefaultPlatformsFactory.cs
+++ Code/Core/Tower/Factory/DefaultPlatformsFactory.cs
@@ -4,11 +4,8 @@ public class DefaultPlatformsFactory : MonoBehaviour, IPoolFactory<Platform>
 {
     [SerializeField] private Platform _prefab = null;
 
-    private int i;
-
     public Platform Create()
     {
-        print($"Платформ: {++i}");
         return Instantiate(_prefab);
     }
 }
